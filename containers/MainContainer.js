8e8532e3cd489319add38e4a66696154291f93ea
konrad <ktalaga90@gmail.com>
2021-06-09 20:11:58 +0100

    MainContainer added and rendered

---
+++ containers/MainContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const MainContainer = () => {
+    
+    return (
+        <View>
+            <Text>I am the MainContainer</Text>
+        </View>
+    );
+
+}
+
+export default MainContainer;
