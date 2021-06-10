abb6a38a9523a0b53bda81f6f8b52e8066d01e8f
EenMacFullstack <Iain.mcdulling@gmail.com>
2021-06-10 14:13:42 +0100

    commit

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -1,45 +1,19 @@
-import React from 'react';
-import {StyleSheet, View, Text, FlatList} from 'react-native';
-import {Link} from 'react-router-dom';
-import {ProfileTabNavigator} from 'react-native'
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import NavigationBar from 'react-native-navigation-bar';
+    
+ const NavBar = () => {
+     return (
+          <View>
+            <NavigationBar 
+              title='Main title'
+              height={50}
+              leftButtonTitle='back'
+              rightButtonTitle='forward'
+            />
+            <Text>ABC</Text>
+          </View>
+        );
+      } 
 
-
-
-const NavBar = () => {
-  
-
-    return (
-        <ProfileTabNavigator tabBarOptions={{
-            labelStyle: {
-                textTransform: "uppercase",
-            },
-            indicatorStyle: {
-                height: null,
-                top: '10%',
-                bottom: '10%',
-                width: '45%',
-                left: '2.5%',
-                borderRadius: 100,
-            },
-            style: {
-                alignSelf: "center",
-                width: '50%',
-                borderRadius: 100,
-                borderColor: "blue",
-                backgroundColor: "white",
-                elevation: 5, // shadow on Android
-                shadowOpacity: .10, // shadow on iOS,
-                shadowRadius: 4, // shadow blur on iOS
-            },
-            tabStyle: {
-                borderRadius: 100,
-            },
-        }}
-        swipeEnabled={true}>
-     <ProfileTabNavigator.Screen name="Info" component={ProfileInfoScreen} />
-     <ProfileTabNavigator.Screen name="Photos" component={ProfilePhotosScreen} />
-   </ProfileTabNavigator>
-  );
-}
-
-export default NavBar;
+      export default NavBar;
